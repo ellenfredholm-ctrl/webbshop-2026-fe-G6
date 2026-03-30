6d0cbfd67e6b45f4042ac9bd9f6cec5e5b9d25dc
ellenfredholm-ctrl <ellen.fredholm@gmail.com>
2026-03-30 11:37:48 +0200

    add correcct backend address to api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 
 export function getBaseUrl() {
-  if (window.location.hostname.includes("localhost")) {
+  if (window.location.hostname.includes("localhost") && false) {
     return "http://localhost:3000/";
   }
   // TODO: Add deployed backend URL
-  return "https://your-backend.vercel.app/";
+  return "https://webb-projekt-2026.vercel.app/";
 }
