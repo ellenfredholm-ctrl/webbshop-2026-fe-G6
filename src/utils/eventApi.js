1c6a14c8f2add844f3eebd68f19269dd5315952b
ellenfredholm-ctrl <ellen.fredholm@gmail.com>
2026-04-17 09:52:02 +0200

    partial update to admin

--- src/utils/eventApi.js
+++ src/utils/eventApi.js
@@ -17,8 +17,23 @@ export async function createEvent(event) {
     body: JSON.stringify(event),
   });
   if (response.ok) {
-    return response.json();
+    return response.json().catch(() => ({}));
   }
   const err = await response.json().catch(() => ({}));
   throw new Error(err.errors?.[0]?.msg || "Failed to create event");
 }
+
+//en updateEvent för redigering av events
+// export async function updateEvent(id, updatedData) {
+//   const url = new URL(`events/${id}`, getBaseUrl());
+//   const response = await fetch(url, {
+//     method: "PUT",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify(updatedData),
+//   });
+//   if (response.ok) {
+//     return response.json().catch(() => ({}));
+//   }
+//   const err = await response.json().catch(() => ({}));
+//   throw new Error(err.errors?.[0]?.msg || "Failed to update event");
+// }
